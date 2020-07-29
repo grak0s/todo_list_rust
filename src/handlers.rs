9d9be5ed09d92244842b4b4ce064db8c0f8b4f5a
Alex Rodriguez Desarrollo <desarrollo@previtest.onmicrosoft.com>
2020-07-29 11:23:30 -0500

    commit

---
+++ src/handlers.rs
@@ -0,0 +1,24 @@
+use crate::models::Status;
+use crate::db;
+use deadpool_postgres::{Pool,Client};
+use actix_web::{web, Responder, HttpResponse};
+
+pub async fn status()-> impl Responder{
+
+    web::HttpResponse::Ok()
+        .json(Status{status:"Up".to_string()})
+}
+
+
+pub async fn get_todos(db_pool: web::Data<Pool>)-> impl Responder{
+    let client:Client=
+        db_pool.get().await.expect("Error conectin database");
+
+    let result= db::get_todos(&client).await;
+
+    match result{
+        Ok(todos)=>HttpResponse::Ok().json(todos),
+        Err(_)=>HttpResponse::InternalServerError().into()
+    }
+
+}
